921fe2b3768e3131409f6f4fa16f5d41793b1007
F1NH4WK <eduardo.ar334@gmail.com>
2026-03-11 11:03:29 -0300

    feat: add verbose flag

---
+++ internal/consumer/writer.go
@@ -0,0 +1,30 @@
+package consumer
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func WriteToDisk(inChan <-chan string, filename string, verbose bool) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+
+	for password := range inChan {
+		// Write in file's buffer
+		writer.WriteString(password + "\n")
+		
+		// Verbose flag (lowest speed)
+		if verbose {
+			fmt.Println(password)
+		}
+	}
+
+	return nil
+}
